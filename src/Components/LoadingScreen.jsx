91e52085a2fbe278306be9940d281eb82d336343
Luis Carlos Rangel <luiscrangel19@gmail.com>
2022-08-09 22:42:17 -0500

    Version 2.0

--- src/Components/LoadingScreen.jsx
+++ src/Components/LoadingScreen.jsx
@@ -4,7 +4,7 @@ const LoadingScreen = () => {
   return (
     <div className="loading-container">
       <div className="card-loading">
-          <h1>Loading...</h1>
+      <h1>Loading...</h1>
           <div className="lds-ring"><div></div><div></div><div></div><div></div></div>          
       </div>
     </div>
